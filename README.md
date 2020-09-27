19f541ae45b92131f89b7228c29fb2a979442ce6
Krishnan Narayana Swamy <krnaraya@redhat.com>
2020-09-27 16:15:17 +1000

    fixed all issues

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-The CCN Roadshow(Dev Track) Module 4 Labs 2019
-===
 
 This repo can be used for reference to the code's solution than a magic script to deploy all things completely at one time because you will have some errors in OpenShift runtime if you don't deploy required pods, objects manually.
--- coolstore-ui/app/services/catalog.js
+++ coolstore-ui/app/services/catalog.js
@@ -5,7 +5,7 @@ angular.module("app")
 .factory('catalog', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
 	var factory = {}, products, baseUrl;
 
-    baseUrl = $location.protocol() + '://catalog-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/products';
+    baseUrl = $location.protocol() + '://catalog-springboot-' + COOLSTORE_CONFIG.OCP_NAMESPACE + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1") + '/api/products';
 
     factory.getProducts = function() {
 		var deferred = $q.defer();
